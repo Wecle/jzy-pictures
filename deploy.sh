2ae089588d7e1013535255a4a485840c23ac5ece
Wecle <837968732@qq.com>
2022-12-10 18:04:33 +0800

    add pictures

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 删除 dist 文件
+# rm -rf dist
+
+# 生成静态文件
+yarn docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Wecle/jzy-pictures.git master:gh-pages
+
+cd -
---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vuepress/config'
+
+export default defineConfig({
+    title: '江子毅作品集',
+    base: '/jzy-pictures/',
+    description: 'A Personal Pictures Repo',
+    themeConfig: {
+        repo: "Wecle/jzy-pictures",
+    }
+})
